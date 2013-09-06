db1fdc2251f1af4956c50cc52d86a7640a5b576d
uwerupp <u_rupp@yahoo.de>
2013-09-06 12:49:55 +0000

    Adding gradder.js and checks.json

--- grader.js
+++ grader.js
@@ -24,8 +24,13 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.coursera.com";
+
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +39,29 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrlExists = function(inUrl) {
+    console.error("inUrl ", inUrl.toString());
+
+    var inUrlContent; 
+	rest.get(inUrl).on('complete', function(result, response) {
+        if (result instanceof Error) {
+            console.error('URL not reachable ' + inUrl);
+            process.exit(1); 
+	    } else {
+            console.error("URL exists %s", inUrl);
+            }
+    inUrlContent = util.format("%s",result); 
+    console.error("inUrlContent " +  inUrlContent);
+//            process.exit(1); 
+//return util.format("%s",result); 
+   } 
+);				       
+
+    console.error("inUrlContent 2 " +  inUrlContent);
+            process.exit(1); 
+    return inUrlContent;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +93,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u --url <url>', 'Url to page', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
